bbc9414a513acd57b8098d58050a7cc310390adc
omeyang <omeyang03@gmail.com>
2023-08-09 16:31:12 +0800

    数组第一天

--- array/day1.go
+++ array/day1.go
@@ -1,7 +1,11 @@
 package array
 
+import "sort"
+
 /*
 1. 两数之和
+时间复杂度  O(n)
+空间复杂度  O(n)
 */
 func twoSum(nums []int, target int) []int {
 	data := make(map[int]int)
@@ -12,4 +16,78 @@ func twoSum(nums []int, target int) []int {
 		data[value] = index
 	}
 	return nil
+}
+
+/*
+11. 盛水最多的容器
+时间复杂度  O(n)
+空间复杂度  O(1)
+*/
+func maxArea(height []int) int {
+	left, right := 0, len(height)-1
+	ans := 0
+	for left < right {
+		area := (right - left) * compareMin(height[left], height[right])
+		ans = compareMax(ans, area)
+		if height[left] < height[right] {
+			left++
+		} else {
+			right--
+		}
+	}
+	return ans
+}
+
+func compareMax(curr, data int) int {
+	if curr > data {
+		return curr
+	}
+	return data
+}
+
+func compareMin(curr, data int) int {
+	if curr > data {
+		return data
+	}
+	return curr
+}
+
+/*
+15. 三数之和
+时间复杂度  O(n+ nlog(n))
+空间复杂度  O(1)
+*/
+func threeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	ans := [][]int{}
+	for index, value := range nums[:len(nums)-2] {
+		if value > 0 {
+			break
+		}
+		if value+nums[index+1]+nums[index+2] > 0 {
+			break
+		}
+		if value+nums[len(nums)-2]+nums[len(nums)-1] < 0 {
+			continue
+		}
+		if index > 0 && value == nums[index-1] { // 跳过重复数字
+			continue
+		}
+
+		left, right := index+1, len(nums)-1
+		for left < right {
+			if value+nums[left]+nums[right] > 0 {
+				right--
+			} else if value+nums[left]+nums[right] < 0 {
+				left++
+			} else {
+				ans = append(ans, []int{value, nums[left], nums[right]})
+				for left++; left < right && nums[left] == nums[left-1]; left++ {
+				}
+				for right--; right > left && nums[right] == nums[right+1]; right-- {
+				}
+			}
+		}
+	}
+	return ans
 }
--- array/day1_test.go
+++ array/day1_test.go
@@ -27,3 +27,47 @@ func Test_twoSum(t *testing.T) {
 		})
 	}
 }
+
+func Test_maxArea(t *testing.T) {
+	type args struct {
+		height []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case 1", args{[]int{1, 8, 6, 2, 5, 4, 8, 3, 7}}, 49},
+		{"case 2", args{[]int{2, 3, 4, 5, 18, 17, 6}}, 17},
+		{"case 3", args{[]int{1, 1}}, 1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxArea(tt.args.height); got != tt.want {
+				t.Errorf("maxArea() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_threeSum(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		{"case1", args{[]int{-1, 0, 1, 2, -1, -4}}, [][]int{{-1, -1, 2}, {-1, 0, 1}}},
+		{"case2", args{[]int{0, 1, 1}}, [][]int{}},
+		{"case3", args{[]int{0, 0, 0}}, [][]int{{0, 0, 0}}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("threeSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
