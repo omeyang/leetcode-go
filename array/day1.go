904914393821f8021ed752a33ea0f03a94caefbc
omeyang <omeyang03@gmail.com>
2023-08-10 11:23:27 +0800

    规整代码

--- array/day1.go
+++ array/day1.go
@@ -1,6 +1,9 @@
 package array
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 /*
 1. 两数之和
@@ -88,4 +91,28 @@ func threeSum(nums []int) (ans [][]int) {
 		}
 	}
 	return ans
+}
+
+/*
+16. 最接近的三数之和
+*/
+func threeSumClosest(nums []int, target int) int {
+	sort.Ints(nums)
+	res := nums[0] + nums[1] + nums[len(nums)-1]
+	for index, value := range nums[:len(nums)-2] {
+		left, right := index+1, len(nums)-1
+		for left < right {
+			sum := value + nums[left] + nums[right]
+			if math.Abs(float64(sum-target)) < math.Abs(float64(res-target)) {
+				res = sum
+			}
+			if sum > target {
+				right--
+			} else {
+				left++
+			}
+
+		}
+	}
+	return res
 }
--- array/day1_test.go
+++ array/day1_test.go
@@ -71,3 +71,48 @@ func Test_threeSum(t *testing.T) {
 		})
 	}
 }
+
+func Test_threeSumClosest(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"case1", args{nums: []int{-1, 2, 1, -4}, target: 1}, 2},
+		{"case2", args{nums: []int{0, 0, 0}, target: 1}, 0},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := threeSumClosest(tt.args.nums, tt.args.target); got != tt.want {
+				t.Errorf("threeSumClosest() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_twoSum2(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"第一条 case 2 5 7 11 15", args{nums: []int{2, 7, 11, 15}, target: 9}, []int{0, 1}},
+		{"第二条 case 3,2,4", args{nums: []int{3, 2, 4}, target: 6}, []int{1, 2}},
+		{"第三条 case 3,3", args{nums: []int{3, 3}, target: 6}, []int{0, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := twoSum2(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("twoSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
